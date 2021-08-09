d77278d86784fabcc7065b3039df4f9370e4f684
unknown <tahalakdawala03@gmail.com>
2021-08-09 15:50:25 +0530

    v1.0

--- MailService/mandrillMailService.py
+++ MailService/mandrillMailService.py
@@ -2,15 +2,16 @@ import requests
 import json
 import os
 
+
 class EmailSenderMandrill:
     """
     This is the email sender class using the MANDRILL implementation
-    """ 
-        
+    """
+
     def send(self, from_email, to_list, cc_list, bcc_list, subject, text):
         """
         To send mail using Mandrill
-        
+
         params:
             -from_mail
             -to_list
@@ -18,70 +19,51 @@ class EmailSenderMandrill:
             -bcc_list(optional)
             -subject
             -text
-        
+
         return 0 if the sending sucess
         return 1 if the sending failed
         return 5 if the configuration is not complete
         """
-        api_url = os.environ['mandrill_api_url']
-        key = os.environ['mandrill_key']
-                
+        api_url = os.environ["mandrill_api_url"]
+        key = os.environ["mandrill_key"]
+
         if not api_url or not key:
-            return 5, 'configuration not complete'
+            return 5, "configuration not complete"
         else:
-            
+
             to_address_list = []
-            
+
             if len(to_list) > 0:
                 for to_address in to_list:
-                    to_address_list.append(
-                        {
-                            "email":to_address,
-                            "type":"to"
-                        }
-                    )
-            
+                    to_address_list.append({"email": to_address, "type": "to"})
+
             if len(cc_list) > 0:
                 for cc_address in cc_list:
-                    to_address_list.append(
-                        {
-                            "email":cc_address,
-                            "type":"cc"
-                        }
-                    )
+                    to_address_list.append({"email": cc_address, "type": "cc"})
 
             if len(bcc_list) > 0:
                 for bcc_address in bcc_list:
-                    to_address_list.append(
-                        {
-                            "email":bcc_address,
-                            "type":"bcc"
-                        }
-                    )
-                        
+                    to_address_list.append({"email": bcc_address, "type": "bcc"})
+
             mandrill_data = {
-                "key":key,
-                "message":{
-                    "text":text,
-                    "subject":subject,
-                    "from_email":from_email,
-                    "to":to_address_list,
-                }
+                "key": key,
+                "message": {
+                    "text": text,
+                    "subject": subject,
+                    "from_email": from_email,
+                    "to": to_address_list,
+                },
             }
 
-            response = requests.post(
-                api_url,
-                data=json.dumps(mandrill_data)
-            )
-            
-               
+            response = requests.post(api_url, data=json.dumps(mandrill_data))
+
             if response.ok:
                 status = 0
             else:
                 status = 1
-                
+
             message = str(response.content)
 
-            print('Mandrill')
-          
+            print("Mandrill")
+
             return status, message
--- service.py
+++ service.py
@@ -3,29 +3,33 @@ from MailService.mandrillMailService import EmailSenderMandrill
 from MailService.mailjetMailService import EmailSenderMailJet
 from MailService.awsMailService import EmailSenderAWS
 
+
 class EmailService:
     """
-    This class is the service interface. 
-    It will 
+    This class is the service interface.
+    It will
     - validate the emails address, subject and email content text
-    - call EmailSender to send the emails. EmailSender is the email service providers. 
-    - failover. EmailSerivce privode the abstract of the email service provides. 
+    - call EmailSender to send the emails. EmailSender is the email service providers.
+    - failover. EmailSerivce privode the abstract of the email service provides.
       If one of the services goes down,  EmailService will quickly failover to a different provider without affecting the customers.
     """
+
     sender_id = 0
-    
+
     def __init__(self):
         self.senderservice = []
         self.senderservice.append(EmailSenderMailJet())
         self.senderservice.append(EmailSenderAWS())
         self.senderservice.append(EmailSenderMandrill())
-    
+
     def validate_email_address(self, email_address):
         """
         Validates Email
         """
-        return re.match(r"[a-zA-Z0-9]+(\.?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.?[a-zA-Z0-9]+)*\.[a-zA-Z0-9]{2,}", email_address)
-            
+        return re.match(
+            r"[a-zA-Z0-9]+(\.?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.?[a-zA-Z0-9]+)*\.[a-zA-Z0-9]{2,}",
+            email_address,
+        )
 
     def list_or_str_to_valid_list(self, list_or_string):
         """
@@ -33,30 +37,29 @@ class EmailService:
         Or if the input is already list, simply return itself
         Meanwhile if the email address is invalid, remove it from the list
         """
-        if isinstance(list_or_string, list):            
-            
+        if isinstance(list_or_string, list):
+
             for email in list_or_string:
                 if not self.validate_email_address(email):
                     list_or_string.remove(email)
             return list_or_string
-            
+
         else:
-            result_list = []            
+            result_list = []
             if self.validate_email_address(list_or_string):
                 result_list.append(list_or_string)
             return result_list
-    
-        
+
     def send_email(self, from_email, to_list, cc_list, bcc_list, subject, text):
         """
         This is to send email by the email sender class and failover.
         It will also validate the from, to email address. Validate the subject and email content text.
         It will return an status code and message
-        
+
         The to_list, cc_list and bcc_list could be None or an empty list [] or a string representing one email address, or a list of email addresses.
         But it should have at least one valid to or cc or bcc email address.
-        
-        status  message  
+
+        status  message
         0       success
         1       from email address invalid
         2       no valid to/cc/bcc email address
@@ -64,50 +67,63 @@ class EmailService:
         4       all email sender failed
         5       configuration not complete
         """
-        
+
         # checking 'from' email address
-        if from_email is None or len(from_email) == 0 or not self.validate_email_address(from_email):
-            return 1, 'from email address invalid'
+        if (
+            from_email is None
+            or len(from_email) == 0
+            or not self.validate_email_address(from_email)
+        ):
+            return 1, "from email address invalid"
 
         # checking 'to' email address
         if to_list is None or len(to_list) == 0:
             to_list = []
-        else: 
+        else:
             to_list = self.list_or_str_to_valid_list(to_list)
-        
+
         # checking 'cc' email address
         if cc_list is None or len(cc_list) == 0:
             cc_list = []
-        else: 
+        else:
             cc_list = self.list_or_str_to_valid_list(cc_list)
-        
+
         # checking 'bcc' email address
         if bcc_list is None or len(bcc_list) == 0:
             bcc_list = []
         else:
             bcc_list = self.list_or_str_to_valid_list(bcc_list)
 
-
         if len(to_list) == 0 and len(cc_list) == 0 and len(bcc_list) == 0:
-            return 2, 'No valid to/cc/bcc email address. Please provide at least one valid to/cc/bcc email address.'            
-            
+            return (
+                2,
+                "No valid to/cc/bcc email address. Please provide at least one valid to/cc/bcc email address.",
+            )
+
         # checking 'subject' and 'text
         if not subject and not text:
-            return 3, 'subject and text both are empty'
+            return 3, "subject and text both are empty"
         elif not subject:
-            subject = ''
+            subject = ""
         elif not text:
-            text = ''
-        
+            text = ""
+
         # Sending email
         for retry in range(len(self.senderservice)):
-            status, message = self.senderservice[EmailService.sender_id].send(from_email, to_list, cc_list, bcc_list, subject, text)
+            status, message = self.senderservice[EmailService.sender_id].send(
+                from_email, to_list, cc_list, bcc_list, subject, text
+            )
             if status == 0:
-                message = 'success'
+                message = "success"
                 break
-            EmailService.sender_id = (EmailService.sender_id + 1) % len(self.senderservice)
+            EmailService.sender_id = (EmailService.sender_id + 1) % len(
+                self.senderservice
+            )
         else:
             status = 4
-            message = 'Emails failed in sending. The error message is as followed:\n' + message
-    
+            message = (
+                "Emails failed in sending. The error message is as followed:\n"
+                + message
+            )
+
         return status, message
